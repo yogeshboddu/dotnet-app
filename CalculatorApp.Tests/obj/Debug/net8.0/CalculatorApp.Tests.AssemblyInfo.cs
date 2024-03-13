392679a3fc8be700a7ce542cc12e7b5a3942f608
Yogesh B <yogesh.boddu@paymentsense.com>
2024-03-13 15:48:52 +0000

    first commit

--- CalculatorApp.Tests/obj/Debug/net8.0/CalculatorApp.Tests.AssemblyInfo.cs
+++ CalculatorApp.Tests/obj/Debug/net8.0/CalculatorApp.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculatorApp.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba539dcd3eb08724208a57480ba33cabde0c74ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9caaaff0dd20e4f648532f3ea1d5736040016d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculatorApp.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculatorApp.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
