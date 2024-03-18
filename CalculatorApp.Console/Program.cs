77be7d9749fe532e2adadb11e9d8f9214ad3e63e
Yogesh B <yogesh.boddu@paymentsense.com>
2024-03-18 16:58:20 +0000

    added api

--- CalculatorApp.Console/Program.cs
+++ CalculatorApp.Console/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using CalculatorApp;
+
+class Program
+{
+    static HttpClient client = new HttpClient();
+
+    static async Task Main()
+    {
+        var response = await GetGoogleAsync();
+        Console.WriteLine(await response.Content.ReadAsStringAsync());
+    }
+
+    static async Task<HttpResponseMessage> GetGoogleAsync()
+    {
+        return await client.GetAsync("http://www.google.com");
+    }
+}
